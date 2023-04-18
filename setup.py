12afa00e1a0b85401a51033bddcd9b5dd5b56bdb
Dinesh <46128375+dhina016@users.noreply.github.com>
2023-04-18 21:52:16 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,29 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+import subprocess
+
+class PostInstallCommand(install):
+    def run(self):
+        install.run(self)
+        subprocess.call(['dms', 'update'])
 
 setup(
     name='dms',
-    version='0.1',
-    py_modules=['dms'],
+    version='1.0',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'dms = dms.main:main',
+        ],
+    },
     install_requires=[
         'requests',
-        'argparse'
+        'argparse',
     ],
-    entry_points='''
-        [console_scripts]
-        dms=dms:main
-    '''
+    cmdclass={
+        'install': PostInstallCommand,
+    },
+    author='dhina016',
+    description='A Docker misconfiguration scanner',
+    url='https://github.com/dhina016/docker-misconfig-scanner',
 )
